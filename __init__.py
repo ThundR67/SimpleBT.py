dcff169405c3ce5e690dab56588ecb8da125acda
Roshan Mehta <sonicroshan122@gmail.com>
2022-11-24 13:45:28 -0800

    add documentation

--- __init__.py
+++ __init__.py
@@ -5,10 +5,16 @@ import quantstats as qs
 import pandas as pd
 from tqdm import tqdm
 
-qs.extend_pandas()
-
 class Backtest:
+    """Class to backtest a daily trading strategy."""
     def __init__(self, data, start_date, end_date, strategy, benchmark_ticker=None):
+        """
+        @data: dict with keys as ticker symbols and values as pandas dataframes with columns "open", "high", "low", "close", "volume"
+        @start_date: start date of backtest
+        @end_date: end date of backtest
+        @strategy: function that takes in a Backtest object and a dict of data and runs the strategy
+        @benchmark_ticker: ticker symbol of benchmark to compare to. Ticker must be in @data
+        """
         self._data = data
         self._start_date = start_date
         self._end_date = end_date
@@ -19,7 +25,11 @@ class Backtest:
         self.holdings = {}
 
     def _update_returns(self, current_date):
-        returns = 0
+        """
+        Update @self._daily_returns based on current holdings
+        from previous date to @current_date.
+        """
+        self._daily_returns.loc[current_date] = 0
 
         for ticker, amount in self.holdings.items():
             loc = self._data[ticker].index.get_loc(current_date)
@@ -27,12 +37,11 @@ class Backtest:
             past_price = self._data[ticker].iloc[loc - 1]["close"]
             change = (current_price - past_price) / past_price
 
-            returns += amount * change
-
-        self._daily_returns.loc[current_date] = returns
+            self._daily_returns.loc[current_date] += amount * change
 
 
     def sell(self, ticker, percent):
+        """Sell @percent of @ticker from current holdings."""
         if percent == 1:
             del self.holdings[ticker]
             return
@@ -40,9 +49,11 @@ class Backtest:
         self.holdings[ticker] -= self.holdings[ticker] * (1 - percent)
 
     def sell_all(self):
+        """Sell all holdings."""
         self.holdings = {}
 
     def buy(self, ticker, percent=1):
+        """Buy @percent of @ticker and rebalances other holdings."""
         if percent == 1:
             self.holdings = {ticker: 1}
             return
@@ -59,6 +70,7 @@ class Backtest:
 
 
     def run(self):
+        """Runs backtest."""
         start_index = self._start_date
         data_to_pass = {ticker:data.loc[:start_index, "close"].to_list() for ticker, data in self._data.items()}
 
@@ -72,5 +84,5 @@ class Backtest:
                 self._strategy(self, data_to_pass)
 
     def report(self):
+        """Generates a HTML report of backtest."""
         qs.reports.html(self._daily_returns, output=True)
-
